a98bbbc9b0d2d2de168870238652583b309202c9
declan ryan <dryan005@gmail.com>
2020-05-17 19:50:52 +0100

    added new url links/views and basic template info

--- django_project/document_classification/urls.py
+++ django_project/document_classification/urls.py
@@ -5,4 +5,14 @@ from . import views
 urlpatterns = [
     
     path('', views.home, name='document_classification-home'),
+    path('choose_classifier', views.choose_classifier, name='document_classification-choose_classifier'),
+    path('import_data_type', views.import_data_type, name='document_classification-import_data_type'),
+    path('model_name', views.model_name, name='document_classification-model_name'),
+    path('preview_data', views.preview_data, name='document_classification-preview_data'),
+    path('results_page', views.results_page, name='document_classification-results_page'),
+    path('setup_complete', views.setup_complete, name='document_classification-setup_complete'),
+    path('tag_selection', views.tag_selection, name='document_classification-tag_selection'),
+    path('testing', views.testing, name='document_classification-testing'),
+    path('upload_confirmation', views.upload_confirmation, name='document_classification-upload_confirmation'),
+    path('upload_file', views.upload_file, name='document_classification-upload_file'),
 ]
--- django_project/document_classification/views.py
+++ django_project/document_classification/views.py
@@ -7,3 +7,47 @@ from django.contrib.auth.models import User
 def home(request):
     
     return render(request, 'document_classification/home.html')
+
+def choose_classifier(request):
+    
+    return render(request, 'document_classification/choose_classifier.html')
+
+def import_data_type(request):
+    
+    return render(request, 'document_classification/import_data_type.html')
+
+def model_name(request):
+    
+    return render(request, 'document_classification/model_name.html')
+
+def preview_data(request):
+    
+    return render(request, 'document_classification/preview_data.html')
+
+def results_page(request):
+    
+    return render(request, 'document_classification/reults_page.html')
+
+def setup_complete(request):
+    
+    return render(request, 'document_classification/setup_complete.html')
+
+def tag_selection(request):
+    
+    return render(request, 'document_classification/tag_selection.html')
+
+def testing(request):
+    
+    return render(request, 'document_classification/testing.html')
+def home(request):
+    
+    return render(request, 'document_classification/home.html')
+
+def upload_confirmation(request):
+    
+    return render(request, 'document_classification/upload_confirmation.html')
+
+def upload_file(request):
+    
+    return render(request, 'document_classification/upload_file.html')
+
